a720321ffd59e221e8cc60d05fa25e64f9e02841
przemek <pwenderholm@gmail.com>
2023-02-18 20:30:23 +0100

    data

--- src/main/java/com/example/loopthymeleaftask/DataController.java
+++ src/main/java/com/example/loopthymeleaftask/DataController.java
@@ -1,2 +1,35 @@
-package com.example.loopthymeleaftask;public class DataController {
+package com.example.loopthymeleaftask;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class DataController {
+    private final DataGeneratorService dataGeneratorService;
+
+    public DataController(DataGeneratorService dataGeneratorService) {
+        this.dataGeneratorService = dataGeneratorService;
+    }
+
+    @PostMapping("/generate-data")
+    String generateData(
+            @RequestParam int size,
+            @RequestParam String language,
+            @RequestParam(required = false) boolean firstName,
+            @RequestParam(required = false) boolean lastName,
+            @RequestParam(required = false) boolean university,
+            @RequestParam(required = false) boolean country,
+            Model model
+    ){
+        Map<String, List<String>> data = dataGeneratorService
+                .generateFakeData(size,language,firstName,lastName,university,country);
+        model.addAttribute("data", data);
+        model.addAttribute("size", size);
+        return "data-table";
+    }
 }
--- src/main/java/com/example/loopthymeleaftask/DataGeneratorService.java
+++ src/main/java/com/example/loopthymeleaftask/DataGeneratorService.java
@@ -1,2 +1,46 @@
-package com.example.loopthymeleaftask;public class DataGeneratorService {
+package com.example.loopthymeleaftask;
+
+import com.github.javafaker.Faker;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+@Service
+public class DataGeneratorService {
+    Map<String, List<String>> generateFakeData(
+            @RequestParam int size,
+            @RequestParam String language,
+            @RequestParam(required = false) boolean firstname,
+            @RequestParam(required = false) boolean lastname,
+            @RequestParam(required = false) boolean university,
+            @RequestParam(required = false) boolean country
+    ){
+        Faker faker = new Faker(new Locale(language));
+        Map<String,List<String>> data = new LinkedHashMap<>();
+        if(firstname){
+            data.put((faker.equals("pl-PL"))?"Imię:":"Name:" , generateData(size, () -> faker.name().firstName()));
+        }
+        if(lastname){
+            data.put((faker.equals("pl-PL"))?"Nazwisko:":"Surname:"  , generateData(size, () -> faker.name().lastName()));
+        }
+        if(university){
+            data.put((faker.equals("pl-PL"))?"Uczelnia:":"University:"  , generateData(size, () -> faker.university().name()));
+        }
+        if(country){
+            data.put((faker.equals("pl-PL"))?"Kraj pochodzenia:":"Country:"  , generateData(size, () -> faker.country().name()));
+        }
+        return data;
+    }
+
+    private List<String> generateData(int size, Supplier<String> supplier){
+        return Stream.generate(supplier)
+                .limit(size)
+                .toList();
+    }
 }
